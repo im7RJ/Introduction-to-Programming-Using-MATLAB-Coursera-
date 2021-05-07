53076d3b6d35cc0bc0fbf491055b07a7700d1bcd
Rutvik Joshi <81458647+im7RJ@users.noreply.github.com>
2021-05-07 19:13:45 +0530

    Create MATLAB as a Calculator.m

---
+++ Introduction to Programming using MATLAB/Assignments/MATLAB as a Calculator.m
@@ -0,0 +1,5 @@
+principal_dollars = 1000;
+S.I.percent = 10;
+Time_yrs = 2;
+amount_after_1Yr = (principal_dollars + ((principal_dollars*S.I.percent)/100));
+debt =  (amount_after_1Yr + ((amount_after_1Yr*S.I.percent)/100))
